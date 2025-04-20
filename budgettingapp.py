d97c1d53114e6ce976b0540a6bab93e4601a1399
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-20 21:39:01 +1000

    Add files via upload

---
+++ budgettingapp.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import time
+
+st.title('New App')
+st.subheader('Budgetting App')
+
+imported_file = st.file_uploader('Upload your CSV file', type='csv')
+if imported_file is not None:
+    df = pd.read_csv(imported_file)
+    st.write('Data Preview:')
+    st.write(df)
+else:
+    st.write('Please upload a CSV file to get started.')
+
+st.write('This is a change')
