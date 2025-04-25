0810df99619e22969606014a3bdaddb8b090d3c8
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-25 13:22:12 +1000

    update

--- budgettingapp.py
+++ budgettingapp.py
@@ -10,11 +10,12 @@ st.title('Budgetting App')
 # Creae a connection to Neon PostgreSQL database
 conn=st.connection("neon",type="sql")
 
+st.write(st.session_state)
+
 def update_session_state_for_buttons():
     if st.session_state.show_data_from_neon1 not in st.session_state:
         st.write("Else Option Selected")
         del st.session_state.show_data_from_neon1
-        del st.session_state.show_data_from_neon
     else:    
         df = conn.query("SELECT * FROM home", ttl="10minutes")
         for row in df.itertuples():
