630b6bc96fac159044d0af433e10bc810a9f8f97
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-25 22:42:00 +1000

    update

--- budgettingapp.py
+++ budgettingapp.py
@@ -4,7 +4,6 @@ import numpy as np
 import time
 import os
 from sqlalchemy import create_engine, text
-import psycopg2
 from sqlalchemy.engine import URL
 
 
@@ -13,12 +12,15 @@ st.title('Budgetting App')
 
 # Creae a connection to Neon PostgreSQL database
 conn=st.connection("neon",type="sql")
-engine = create_engine(conn)
-with conn.session as session:
-    st.subheader('Upload your CSV file')
-    imported_file = st.file_uploader('', type='csv')
-    if imported_file is not None:
-        df = pd.read_csv(imported_file)
-        st.write('Data Preview:')
-        st.write(df)
-        df.to_sql('name', engine, if_exists='append', index=False)
+df = conn.query("SELECT * FROM home")
+st.dataframe(df)
+
+
+# with conn.session as session:
+#     st.subheader('Upload your CSV file')
+#     imported_file = st.file_uploader('', type='csv')
+#     if imported_file is not None:
+#         df = pd.read_csv(imported_file)
+#         st.write('Data Preview:')
+#         st.write(df)
+#         df.to_sql('name', engine, if_exists='append', index=False)
