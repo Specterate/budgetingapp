b796a80c98e4cc9ac9b6c4676d55ff2d59bc88ed
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-25 20:04:39 +1000

    updated

--- budgettingapp.py
+++ budgettingapp.py
@@ -14,17 +14,44 @@ st.title('Budgetting App')
 # Creae a connection to Neon PostgreSQL database
 conn=st.connection("neon",type="sql")
 
-button_insert_query = st.button('Insert Query', key='insert_query')
-if button_insert_query:
-    # Insert a row into the table
-    with conn.session as session:
-        df = session.execute(text("""
-                                INSERT INTO home (name, pet) 
-                                VALUES ('tyler', 'jack')
-                                """))
-        session.commit()
-    st.write("Row inserted successfully!")     
+# button_insert_query = st.button('Insert Query', key='insert_query')
+# if button_insert_query:
+#     # Insert a row into the table
+#     with conn.session as session:
+#         df = session.execute(text("""
+#                                 INSERT INTO home (name, pet) 
+#                                 VALUES ('tyler', 'jack')
+#                                 """))
+#         session.commit()
+#     st.write("Row inserted successfully!")     
         
+
+# st.subheader('Upload your CSV file')
+# imported_file = st.file_uploader('', type='csv')
+# if imported_file is not None:
+#     df = pd.read_csv(imported_file)
+#     st.write('Data Preview:')
+#     st.write(df)
+#     for row in df.iterrows():
+#         name, pet = str(f'{row[0]}'), str(f'{row[1]}')
+#         st.write(f'Name: {name}, Pet: {pet}')
+#     # Insert the values into the database
+#         with conn.session as session:
+#             new_data = (name, pet)
+#             st.write(f'The new data is {new_data}')
+#             st.write(f'Waiting for 5 seconds....') 
+#             time.sleep(5)
+#             # Define the SQL query to insert the data
+#             query = text("""
+#             INSERT INTO home ("name", "pet") 
+#             VALUES (:name, :pet)
+#             """)
+#             session.execute(text(query), {'name': name, 'pet': pet})
+#             # Commit the transaction
+#             session.commit()
+#             st.write("Row inserted successfully!")
+# else:
+#     st.write('Warning: Please upload a CSV file to get started.')
 
 st.subheader('Upload your CSV file')
 imported_file = st.file_uploader('', type='csv')
@@ -32,23 +59,4 @@ if imported_file is not None:
     df = pd.read_csv(imported_file)
     st.write('Data Preview:')
     st.write(df)
-    for row in df.iterrows():
-        name, pet = str(f'{row[0]}'), str(f'{row[1]}')
-        st.write(f'Name: {name}, Pet: {pet}')
-    # Insert the values into the database
-        with conn.session as session:
-            new_data = (name, pet)
-            st.write(f'The new data is {new_data}')
-            st.write(f'Waiting for 5 seconds....') 
-            time.sleep(5)
-            # Define the SQL query to insert the data
-            query = text("""
-            INSERT INTO home ("name", "pet") 
-            VALUES (:name, :pet)
-            """)
-            session.execute(text(query), {'name': name, 'pet': pet})
-            # Commit the transaction
-            session.commit()
-            st.write("Row inserted successfully!")
-else:
-    st.write('Warning: Please upload a CSV file to get started.')
+    df.to_sql('name', conn, if_exists='append', index=False)
