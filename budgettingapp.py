e31f444ab71e80955b7e79a1a2e57680cedc0d28
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-25 15:45:31 +1000

    update

--- budgettingapp.py
+++ budgettingapp.py
@@ -10,16 +10,6 @@ st.title('Budgetting App')
 # Creae a connection to Neon PostgreSQL database
 conn=st.connection("neon",type="sql")
 
-st.write(st.session_state)
-
-def update_session_state_for_buttons():
-    df = conn.query("SELECT * FROM home", ttl="10minutes")
-    for row in df.itertuples():
-        st.write(f"Row {row.name}: {row.pet}")
-
-def reset():
-    del st.session_state.show_data_from_neon1
-         
 file_upload = st.button("Click to upload a CSV file", key="file_upload")
 if file_upload:
     st.subheader('Upload your CSV file')
@@ -31,14 +21,3 @@ if file_upload:
     else:
         st.write('Warning: Please upload a CSV file to get started.')
 
-show_data_from_neon = st.button("Click to show data from Neon", key="show_data_from_neon1", on_click=update_session_state_for_buttons)
-reset_data = st.button("Click to reset data", key="reset_data", on_click=reset)
-
-# Add a button to the app
-# button_clicked = st.button("click me")
-# if button_clicked:
-#     button_clicked_count += 1
-#     st.write('I love you my mangu')
-#     st.write(button_clicked_count)
-
-st.write(st.session_state)
