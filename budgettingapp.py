6017199787b42133e87ca34b93a946efa35f3678
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 09:11:56 +1000

    Update

--- budgettingapp.py
+++ budgettingapp.py
@@ -13,11 +13,17 @@ st.title('Budgetting App')
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
 
-# Add new name to the database
-capture_new_name = st.text_input("Enter person name", help="Enter name")
-capture_new_pet = st.text_input("Enter pet name", help="Enter pet name")
-st.write(f'The name entered is {capture_new_name}')
-st.write(f'The pet entered is {capture_new_pet}')
+col1, col2 = st.columns(2, bordered=True)
+with col1:
+    # Add new name to the database
+    capture_new_name = st.text_input("Enter person name", help="Enter name")
+    st.write(f'The name entered is {capture_new_name}')
+with col2:
+    capture_new_pet = st.text_input("Enter pet name", help="Enter pet name")
+    st.write(f'The pet entered is {capture_new_pet}')
+
+
+
 if st.button('Add Name and Pet'):
     response = (
         conn.table("mytable")
