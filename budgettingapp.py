8038a4f8dd8062fa94ed6a9cd5104e1b27cf3512
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 21:48:52 +1000

    Updated

--- budgettingapp.py
+++ budgettingapp.py
@@ -10,24 +10,25 @@ from st_supabase_connection import SupabaseConnection
 st.set_page_config(page_title="Budgeting App", page_icon="💰", layout="centered")
 st.title("Budgeting App")
 
-@st.cache_data
-def load_data():
-    data = {
-        "Animal": ["Lion", "Crocodile", "Elephant", "Giraffe", "Penguin"],
-        "Weight (kg)": [190, 430, 5000, 800, 4],
-        "Is Endangered": [True, True, True, False, False],
-        "Classification": ["Mammal", "Reptile", "Mammal", "Mammal", "Bird"],
-        "Average Lifespan (years)": [12, 70, 70, 25, 20],
-        "Habitat": ["Grassland", "Water", "Savannah", "Savannah", "Antarctica"],
-    }
-    df = pd.DataFrame(data)
-    df["Classification"] = df["Classification"].astype("category")
-    df["Habitat"] = df["Habitat"].astype("category")
-    return df
+# https://github.com/streamlit/docs/blob/main/python/api-examples-source/data.data_editor4.py
+# @st.cache_data
+# def load_data():
+#     data = {
+#         "Animal": ["Lion", "Crocodile", "Elephant", "Giraffe", "Penguin"],
+#         "Weight (kg)": [190, 430, 5000, 800, 4],
+#         "Is Endangered": [True, True, True, False, False],
+#         "Classification": ["Mammal", "Reptile", "Mammal", "Mammal", "Bird"],
+#         "Average Lifespan (years)": [12, 70, 70, 25, 20],
+#         "Habitat": ["Grassland", "Water", "Savannah", "Savannah", "Antarctica"],
+#     }
+#     df = pd.DataFrame(data)
+#     df["Classification"] = df["Classification"].astype("category")
+#     df["Habitat"] = df["Habitat"].astype("category")
+#     return df
 
 
-df = load_data()
+# df = load_data()
 
-st.data_editor(df, key="my_key", num_rows="dynamic")
-st.write("Here's the value in Session State:")
-st.write(st.session_state["my_key"])
+# st.data_editor(df, key="my_key", num_rows="dynamic")
+# st.write("Here's the value in Session State:")
+# st.write(st.session_state["my_key"])
