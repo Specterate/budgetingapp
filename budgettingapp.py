3c2b21a6f65e61a0218f264616195ef4957caad4
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 19:00:33 +1000

    Update

--- budgettingapp.py
+++ budgettingapp.py
@@ -1,4 +1,11 @@
+import pandas as pd
 import streamlit as st
+import numpy as np
+import time
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+from st_supabase_connection import SupabaseConnection
 
 st.set_page_config(page_title="Budgeting App", page_icon="💰", layout="centered")
 st.title("Budgeting App")
--- pages/categories.py
+++ pages/categories.py
@@ -1,4 +1,19 @@
+import pandas as pd
 import streamlit as st
+import numpy as np
+import time
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+from st_supabase_connection import SupabaseConnection
 
 st.set_page_config(page_title="Categories", page_icon="📚")
 st.title("Categories")
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+st.write('Categories Preview:')
+rows = conn.table("categories").select("*").execute()
+for row in rows.data:
+    st.data_editor(row)
