ddab09a9af58996e5687050914612f7e16f05a6b
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 19:55:44 +1000

    Update

--- pages/categories.py
+++ pages/categories.py
@@ -6,7 +6,7 @@ import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
 from st_supabase_connection import SupabaseConnection
-import st_aggrid as AgGrid
+
 
 st.set_page_config(page_title="Categories", page_icon="📚")
 st.title("Categories")
@@ -17,31 +17,32 @@ conn = st.connection("supabase",type=SupabaseConnection)
 st.write('Categories Preview:')
 rows = conn.table("categories").select("*").execute()
 new_row = pd.DataFrame.from_dict(rows.data)
-AgGrid(new_row)
-# st.data_editor(
-#                 new_row,
-#                 column_order=['category', 'subcategory', 'monthly', 'yearly'],
-#                 column_config={
-#                 "monthly": st.column_config.NumberColumn(
-#                 "Monthly",
-#                 format="dollar"
-#             ),
-#                 "yearly": st.column_config.NumberColumn(
-#                 "Yearly",
-#                 format="dollar"
-#             ),
-#             },
-#                hide_index=True,
-#                num_rows="dynamic",
-#                height=500,
-#                )
+st.session_state = st.data_editor(
+                new_row,
+                column_order=['category', 'subcategory', 'monthly', 'yearly'],
+                column_config={
+                "monthly": st.column_config.NumberColumn(
+                "Monthly",
+                format="dollar"
+            ),
+                "yearly": st.column_config.NumberColumn(
+                "Yearly",
+                format="dollar"
+            ),
+            },
+               hide_index=True,
+               num_rows="dynamic",
+               height=500,
+               )
+st.write(st.session_state)
 
-with st.form(key='add_category_form'):
-    category = st.text_input("Category")
-    subcategory = st.text_input("Subcategory")
-    monthly = st.number_input("Monthly", min_value=0)
-    yearly = st.number_input("Yearly", min_value=0)
-    submit_button = st.form_submit_button(label='Add Category')
-    if submit_button:
-        conn.table("categories").insert({"category": category, "subcategory": subcategory, "monthly": monthly, "yearly": yearly}).execute()
-        st.success(f"Category {category} added successfully!")
+# Using a Form to add a new category
+# with st.form(key='add_category_form'):
+#     category = st.text_input("Category")
+#     subcategory = st.text_input("Subcategory")
+#     monthly = st.number_input("Monthly", min_value=0)
+#     yearly = st.number_input("Yearly", min_value=0)
+#     submit_button = st.form_submit_button(label='Add Category')
+#     if submit_button:
+#         conn.table("categories").insert({"category": category, "subcategory": subcategory, "monthly": monthly, "yearly": yearly}).execute()
+#         st.success(f"Category {category} added successfully!")
