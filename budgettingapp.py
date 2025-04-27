b62255345ca57a3b73f4c6ab6fc46f3e54baf09c
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 18:48:17 +1000

    Update

---
+++ budgettingapp.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.page_config(page_title="Budgeting App", page_icon="💰", layout="centered")
---
+++ pages/categories.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.page_config(page_title="Categories", page_icon="📚")
