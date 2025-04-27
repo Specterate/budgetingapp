f3eba9dc80b175fe68aecf15128dd095e3130896
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 19:50:17 +1000

    Update

--- pages/categories.py
+++ pages/categories.py
@@ -6,6 +6,7 @@ import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
 from st_supabase_connection import SupabaseConnection
+import st.aggrid as AgGrid
 
 st.set_page_config(page_title="Categories", page_icon="📚")
 st.title("Categories")
@@ -16,23 +17,24 @@ conn = st.connection("supabase",type=SupabaseConnection)
 st.write('Categories Preview:')
 rows = conn.table("categories").select("*").execute()
 new_row = pd.DataFrame.from_dict(rows.data)
-st.data_editor(
-                new_row,
-                column_order=['category', 'subcategory', 'monthly', 'yearly'],
-                column_config={
-                "monthly": st.column_config.NumberColumn(
-                "Monthly",
-                format="dollar"
-            ),
-                "yearly": st.column_config.NumberColumn(
-                "Yearly",
-                format="dollar"
-            ),
-            },
-               hide_index=True,
-               num_rows="dynamic",
-               height=500,
-               )
+AgGrid(new_row)
+# st.data_editor(
+#                 new_row,
+#                 column_order=['category', 'subcategory', 'monthly', 'yearly'],
+#                 column_config={
+#                 "monthly": st.column_config.NumberColumn(
+#                 "Monthly",
+#                 format="dollar"
+#             ),
+#                 "yearly": st.column_config.NumberColumn(
+#                 "Yearly",
+#                 format="dollar"
+#             ),
+#             },
+#                hide_index=True,
+#                num_rows="dynamic",
+#                height=500,
+#                )
 
 with st.form(key='add_category_form'):
     category = st.text_input("Category")
