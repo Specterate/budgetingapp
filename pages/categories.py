57ec3cbf0fe20669ad639b19d3788d94f46fa190
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 23:05:08 +1000

    Updated

--- pages/categories.py
+++ pages/categories.py
@@ -63,4 +63,3 @@ with col2:
             conn.table("categories").delete().eq("subcategory", delete_subcategory).execute()
             st.success(f"Category {delete_subcategory} deleted successfully!")
 
-get_data()
