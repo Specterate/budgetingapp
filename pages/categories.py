3eaacea66856570ce99432f30a4ac8b5ff3dd265
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-27 22:26:20 +1000

    Update

--- pages/categories.py
+++ pages/categories.py
@@ -7,45 +7,53 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
 from st_supabase_connection import SupabaseConnection
 
-
+# set page configuration and title
 st.set_page_config(page_title="Categories", page_icon="📚")
 st.title("Categories")
 
-# Initialize connection.
+# Initialize connection with Supabase
 conn = st.connection("supabase",type=SupabaseConnection)
 
-def update_categories():
-    conn.table("categories").upsert(st.session_state).execute()
+st.subheader('Categories Preview:', divider=True)
 
-st.write('Categories Preview:')
+# Get all the categories from the Supabase
 rows = conn.table("categories").select("*").execute()
+
+# Convert the data into a Pandas DataFrame
 new_row = pd.DataFrame.from_dict(rows.data)
 
 st.data_editor(
     new_row,    
-    column_config={
+    column_config=
+    {
         "category": st.column_config.TextColumn("Category"),
         "subcategory": st.column_config.TextColumn("Subcategory"),
         "monthly": st.column_config.NumberColumn("Monthly", format="dollar"),
         "yearly": st.column_config.NumberColumn("Yearly", format="dollar"),
     },               
-    # hide_index=True,
+    hide_index=True,
     num_rows="dynamic",
     height=500,
-    key="my_categories1",
-    # on_change=update_categories,
 )
                
-st.write("Here's the value in Session State:")
-st.write(st.session_state.my_categories1)
+col1, col2 = st.columns(2)
+with col1:
+    # Using a Form to add a new category
+    with st.form(key='add_category_form'):
+        category = st.text_input("Category")
+        subcategory = st.text_input("Subcategory")
+        monthly = st.number_input("Monthly", min_value=0)
+        yearly = st.number_input("Yearly", min_value=0)
+        submit_button = st.form_submit_button(label='Add Category')
+        if submit_button:
+            conn.table("categories").insert({"category": category, "subcategory": subcategory, "monthly": monthly, "yearly": yearly}).execute()
+            st.success(f"Category {category} added successfully!")
 
-# Using a Form to add a new category
-# with st.form(key='add_category_form'):
-#     category = st.text_input("Category")
-#     subcategory = st.text_input("Subcategory")
-#     monthly = st.number_input("Monthly", min_value=0)
-#     yearly = st.number_input("Yearly", min_value=0)
-#     submit_button = st.form_submit_button(label='Add Category')
-#     if submit_button:
-#         conn.table("categories").insert({"category": category, "subcategory": subcategory, "monthly": monthly, "yearly": yearly}).execute()
-#         st.success(f"Category {category} added successfully!")
+with col2:
+    # Delete Sub Category
+    with st.form(key='delete_category_form'):
+        delete_subcategory = st.selectbox("Select Sub Category to Delete", new_row['subcategory'].unique())
+        delete_button = st.form_submit_button(label='Delete Sub Category')
+        if delete_button:
+            conn.table("categories").delete().eq("subcategory", delete_subcategory).execute()
+            st.success(f"Category {delete_subcategory} deleted successfully!")
