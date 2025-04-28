9409114ddce253ef11f955fa4955d5fe3f7ac951
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-28 22:14:30 +1000

    Updated

--- pages/forms.py
+++ pages/forms.py
@@ -23,15 +23,21 @@ if "ss_df" not in st.session_state:
 st.write("This is the session state")
 st.session_state
 
-def update_ss():
+def update_ss(age, name, location):
+    st.session_state.name = name
+    st.session_state.age = age
+    st.session_state.location = location
     new_row_df = pd.DataFrame.from_dict([{"name": st.session_state.name, "age": st.session_state.age, "location": st.session_state.location}])
     st.session_state.ss_df = pd.concat([st.session_state.ss_df, new_row_df], ignore_index=True)
     
 
 with st.form("my_form", clear_on_submit=True, border=True):
         st.write("Inside the form")
-        st.session_state.name = st.text_input("Name", placeholder="Enter your name")
-        st.session_state.age = st.number_input("Age", min_value=0, max_value=100)
-        st.session_state.location = st.selectbox("Location", ["New York", "San Francisco", "Chicago", "Seattle"])
-        submitted = st.form_submit_button("Submit", type="primary", on_click=update_ss)
+        # st.session_state.name = st.text_input("Name", placeholder="Enter your name")
+        # st.session_state.age = st.number_input("Age", min_value=0, max_value=100)
+        # st.session_state.location = st.selectbox("Location", ["New York", "San Francisco", "Chicago", "Seattle"])
+        name = st.text_input("Name", placeholder="Enter your name", key="name")
+        age = st.number_input("Age", min_value=0, max_value=100, key="age")
+        location = st.selectbox("Location", ["New York", "San Francisco", "Chicago", "Seattle"], key="location")
+        submitted = st.form_submit_button("Submit", type="primary", on_click=update_ss, args=(name, age, location))
         
