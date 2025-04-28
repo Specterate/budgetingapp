c00f834ac3e4dfdc3a9422e8ad0721c13356b749
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-28 21:32:38 +1000

    Updated

--- pages/forms.py
+++ pages/forms.py
@@ -30,9 +30,7 @@ with st.form("my_form", clear_on_submit=True, border=True):
     location = st.selectbox("Location", ["New York", "San Francisco", "Chicago", "Seattle"])
     submitted = st.form_submit_button("Submit", type="primary")
     if submitted:
-        st.session_state.ss_df = st.session_state.ss_df.append(
-            {"name": name, "age": age, "location": location}, ignore_index=True
-        )
+        test_df = test_df.append({"name": name, "age": age, "location": location}, ignore_index=True)
         st.success("Form submitted successfully!")
         st.write(st.session_state.ss_df)
         st.balloons()
