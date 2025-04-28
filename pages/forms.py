8770066be0cc8fcc49881f143d687ddf42488ef0
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-28 21:15:48 +1000

    Updated

---
+++ pages/forms.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+from st_supabase_connection import SupabaseConnection
+
+
+test_dict = {
+                "name:" : "John", "age" : 30, "city" : "New York",
+                "name:" : "Jane", "age" : 25, "city" : "Los Angeles",
+                "name:" : "Mike", "age" : 35, "city" : "Chicago",
+                "name:" : "Emily", "age" : 28, "city" : "Houston",
+             }
+
+test_df = pd.DataFrame.from_dict(test_dict, orient='index')
+
+st.write(test_df)
