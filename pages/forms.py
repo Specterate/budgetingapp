60b4f7177c0afa12439675c6ce769128b4db6286
Specterate <53328693+Specterate@users.noreply.github.com>
2025-04-28 21:41:03 +1000

    Update

--- pages/forms.py
+++ pages/forms.py
@@ -32,6 +32,7 @@ with st.form("my_form", clear_on_submit=True, border=True):
     if submitted:
         new_row_df = pd.DataFrame.from_dict({"0":{"name": name, "age": age, "location": location}})
         test_df = pd.concat([test_df,new_row_df], ignore_index=True)
-        st.success("Form submitted successfully!")        
+        st.session_state.ss_df = test_df
+        st.success("Form submitted successfully!")                
     else:
         st.warning("Please fill out the form and submit.")
