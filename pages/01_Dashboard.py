f5c030a95874cc49c4b2f37b91cb500854941d89
falvares <falvares@git.com>
2025-05-04 17:09:07 +1000

    Update

--- pages/01_Dashboard.py
+++ pages/01_Dashboard.py
@@ -11,18 +11,12 @@ from st_supabase_connection import SupabaseConnection, execute_query
 st.set_page_config(page_title="Dashboard", page_icon="📊")
 st.title("Dashboard")
 
-
-
 def refresh_dashboard():
     for key in st.session_state.keys():
         if key == 'user_email':
             pass
         else:
             del st.session_state[key]
-    #  del st.session_state.dashboard_get_category_data_df_ss
-    #  del st.session_state.debit_sum
-    #  del st.session_state.credit_sum
-    #  st.rerun()
 
 if "user_email" not in st.session_state or st.session_state.user_email is None:
     st.write("User is not logged in")
@@ -46,28 +40,25 @@ else:
         dashboard_get_category_data_df = pd.DataFrame.from_dict(dashboard_get_category_data.data)
         st.session_state.dashboard_get_category_data_df_ss = dashboard_get_category_data_df
     
-    st.dataframe(st.session_state.dashboard_get_category_data_df_ss.style.format({"monthly": "${:,.2f}", "yearly": "${:,.2f}"}), hide_index=True, use_container_width=True)
+    # st.dataframe(st.session_state.dashboard_get_category_data_df_ss.style.format({"monthly": "${:,.2f}", "yearly": "${:,.2f}"}), hide_index=True, use_container_width=True)
+
+    debit_sum = st.session_state.dashboard_get_category_data_df_ss.loc[st.session_state.dashboard_get_category_data_df_ss['categorytype']=='Debit','monthly'].sum()
+    if "debit_sum" not in st.session_state:
+        st.session_state.debit_sum = debit_sum
+
+    credit_sum = st.session_state.dashboard_get_category_data_df_ss.loc[st.session_state.dashboard_get_category_data_df_ss['categorytype']=='Credit','monthly'].sum()
+    if "credit_sum" not in st.session_state:
+        st.session_state.credit_sum = credit_sum
+
+    balance = credit_sum - debit_sum
+    if "balance" not in st.session_state:
+        st.session_state.balance = balance
 
     col1, col2, col3 = st.columns(3, border=True)
-    with col1:
-        st.write("Total Debit")
-        debit_sum = st.session_state.dashboard_get_category_data_df_ss.loc[st.session_state.dashboard_get_category_data_df_ss['categorytype']=='Debit','monthly'].sum()
-        if "debit_sum" not in st.session_state:
-            st.session_state.debit_sum = debit_sum
-        st.write(f'$ {st.session_state.debit_sum:,.2f}')
-    with col2:
-        st.write("Total Credit")
-        credit_sum = st.session_state.dashboard_get_category_data_df_ss.loc[st.session_state.dashboard_get_category_data_df_ss['categorytype']=='Credit','monthly'].sum()
-        if "credit_sum" not in st.session_state:
-            st.session_state.credit_sum = credit_sum
-        st.write(f'$ {st.session_state.credit_sum:,.2f}')
-    with col3:
-        st.write("Balance")
-        balance = credit_sum - debit_sum
-        if "balance" not in st.session_state:
-            st.session_state.balance = balance
-        st.write(f'$ {st.session_state.balance:,.2f}')
+    col1.metric(label="Total Debit", value=f'$ {st.session_state.debit_sum:,.2f}')
+    col2.metric(label="Total Credit", value=f'$ {st.session_state.credit_sum:,.2f}')
+    col3.metric(label="Balance", value=f'$ {st.session_state.balance:,.2f}')
 
     st.button("Refresh", type="primary", use_container_width=True, on_click=refresh_dashboard)
 
-    st.session_state
+    # st.session_state
--- pages/03_ImportCSV.py
+++ pages/03_ImportCSV.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
 from st_supabase_connection import SupabaseConnection, execute_query
 
-st.set_page_config(page_title='Budgetting App', page_icon=':moneybag:')
+st.set_page_config(page_title='Import/Export')
 st.title('Expenses')
 
 # Initialize connection.
@@ -23,32 +23,5 @@ if add_df_data:
     # Add DataFrame to the database
     conn.table("mytable1").insert(dataframe.to_dict(orient='records')).execute()
     st.success('DataFrame added to the database!')     
-
-if st.button('Add manual row'):
-    col1, col2 = st.columns(2, border=True)
-    with col1:
-    # Add new name to the database
-        capture_new_name = st.text_input("Enter person name", help="Enter name")
-        st.write(f'The name entered is {capture_new_name}')
-    with col2:
-        capture_new_pet = st.text_input("Enter pet name", help="Enter pet name")
-        st.write(f'The pet entered is {capture_new_pet}')
-
-
-    if st.button('Add Name and Pet'):
-        response = (
-            conn.table("mytable1")
-            .insert({"name": capture_new_name, 
-                    "pet": capture_new_pet})
-            .execute()
-        )
-
-show_data = st.button('Show Data')
-if show_data:
-    st.write('Data Preview:')
-    # Perform query.
-    rows = conn.table("mytable1").select("*").execute()
-    for row in rows.data:
-        st.write(row)
 
 
