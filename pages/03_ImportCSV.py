21073d2ecdaa0f43ce28a4d9e98121802944a6aa
Floyd Alvares <floyd_alvares@hotmail.com>
2025-05-04 18:02:37 +1000

    Update

--- pages/03_ImportCSV.py
+++ pages/03_ImportCSV.py
@@ -10,18 +10,37 @@ from st_supabase_connection import SupabaseConnection, execute_query
 st.set_page_config(page_title='Import/Export')
 st.title('Expenses')
 
-# Initialize connection.
-conn = st.connection("supabase",type=SupabaseConnection)
+# set page configuration and title
+st.set_page_config(page_title="Dashboard", page_icon="📊")
+st.title("Dashboard")
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-   dataframe = pd.read_csv(uploaded_file)
-   st.data_editor(dataframe)
+def refresh_dashboard():
+    for key in st.session_state.keys():
+        if key == 'user_email':
+            pass
+        else:
+            del st.session_state[key]
 
-add_df_data = st.button('Add DataFrame to Database')
-if add_df_data:
-    # Add DataFrame to the database
-    conn.table("mytable1").insert(dataframe.to_dict(orient='records')).execute()
-    st.success('DataFrame added to the database!')     
+if "user_email" not in st.session_state or st.session_state.user_email is None:
+    st.write("User is not logged in")
+    if st.button("Go to Login Page", type="primary"):
+        # Redirect to login page
+        st.switch_page("Budgettingapp.py")
+else:
+    # Set Supabase connection and session state
+    if 'conn' not in st.session_state:
+        conn = st.connection("supabase",type=SupabaseConnection)
+        st.session_state.conn = conn
+
+      uploaded_file = st.file_uploader("Choose a file")
+      if uploaded_file is not None:
+         dataframe = pd.read_csv(uploaded_file)
+         st.data_editor(dataframe)
+
+      add_df_data = st.button('Add DataFrame to Database')
+      if add_df_data:
+         # Add DataFrame to the database
+         conn.table("mytable1").insert(dataframe.to_dict(orient='records')).execute()
+         st.success('DataFrame added to the database!')     
 
 
