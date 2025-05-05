323da73a0fde256d05a50fd54f4425ddaca134d0
Specterate <53328693+Specterate@users.noreply.github.com>
2025-05-05 22:46:06 +1000

    Update 03_ImportCSV.py

--- pages/03_ImportCSV.py
+++ pages/03_ImportCSV.py
@@ -41,48 +41,44 @@ else:
       file_import_df = file_import_df.fillna(value="NA")
       minimum_date = file_import_df['date'].min()
       maximum_date = file_import_df['date'].max()
-      
-      # Get the minimum of the file_month
-      # file_import_min_month = pd.Timestamp(minimum_date).month
-
-      # Get the maximum of the file_month
-      # file_import_max_month = pd.Timestamp(maximum_date).month
-
-      # Query Data from Transaction Table
-      get_data_from_transactions = st.session_state.conn.table("transactions").select("*").gte("date", minimum_date).execute()
-      get_data_from_transactions_df = pd.DataFrame.from_dict(get_data_from_transactions.data)
 
       # Query Data from Category Table
       get_data_from_categories = st.session_state.conn.table("categories").select("subcategory").execute()
       get_data_from_categories_df = pd.DataFrame.from_dict(get_data_from_categories.data)
       key = 'subcategory'
       list_of_subcategories = [d.get(key) for d in get_data_from_categories.data if key in d]
-      
-      # drop index column for get_data_from_transactions_df
-      get_data_from_transactions_df_no_index = get_data_from_transactions_df.drop(columns=['id'])
 
-      # st.dataframe(get_data_from_transactions_df_no_index)
+      # Query Data from Transaction Table
+      get_data_from_transactions = st.session_state.conn.table("transactions").select("*").gte("date", minimum_date).execute()
+      if get_data_from_transactions.data == []:
+         st.write("no data")
+         get_data_from_transactions_df_no_index = pd.DataFrame(columns=['date', 'accounttype', 'description', 'categorytype', 'subcategory', 'amount'])
+      else:
+         get_data_from_transactions_df = pd.DataFrame.from_dict(get_data_from_transactions.data)
+         # drop index column for get_data_from_transactions_df
+         get_data_from_transactions_df_no_index = get_data_from_transactions_df.drop(columns=['id'])
 
       full_df = pd.concat([get_data_from_transactions_df_no_index, file_import_df], ignore_index=True)
-      # st.dataframe(full_df)
 
+      # drop duplicates
       full_df_drop_duplicates = full_df.drop_duplicates()
-
-      # st.write('Updated')
-      # st.dataframe(full_df_drop_duplicates)
+      print(full_df_drop_duplicates.dtypes)
 
       merged_df = pd.merge(get_data_from_transactions_df_no_index, full_df_drop_duplicates, on=['date', 'accounttype', 'description', 'categorytype', 'subcategory', 'amount'], how='right', indicator=True)
-      # st.dataframe(merged_df)
       result_df = merged_df[merged_df['_merge'] == 'right_only'].drop(columns=['_merge'])
-      st.data_editor(result_df,     column_config={
-        "subcategory": st.column_config.SelectboxColumn(
-            "App Category",
-            help="The category of the app",
+      st.data_editor(
+         result_df, 
+         use_container_width=True,
+         column_config={
+         "subcategory": st.column_config.SelectboxColumn(
+           "Sub Category Category",
+            help="Add Subcateogry",
             width="medium",
             options=list_of_subcategories,
             required=True,
-        )
-    },)
+        ), 
+        }
+        ,)
 
       add_df_data = st.button('Import')
       if add_df_data:
