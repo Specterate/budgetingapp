d509c53bee5bb8f28a492d9dea7fff330284113d
Specterate <floyd_alvares@hotmail.com>
2025-05-12 17:27:58 +1000

    Update testingqueries.py

--- pages/testingqueries.py
+++ pages/testingqueries.py
@@ -33,40 +33,40 @@ def openai_classification(desc):
         messages=[
             {"role": "system", "content": content_system},
             {"role": "user", "content": "Expense description: " + str(desc)}]
-)
+)      
+    data = completion.choices[0].message
+    print("data")
+    print(data.content)
+    return data.content
+    
 
 with st.sidebar:
     st.button("Refresh Page", type="primary", use_container_width=True, on_click=refresh_dashboard)
 
 
- 
-# completion = client.chat.completions.create(
-#   model="gpt-4o-mini",
-#   store=True,
-#   messages=[
-#     {"role": "user", "content": "write a haiku about ai"}
-#   ]
-# )
+# uploaded_file = st.file_uploader("Please ensure the file is in CSV format and contains the required columns as Date, Description, Amount", label_visibility ="visible", help="Upload a CSV file with the required columns", key='uploaded_file')
+# if uploaded_file is not None:
+#     try:
+#         file_import_df = pd.read_csv(uploaded_file, usecols=['Date', 'Description', 'Amount'])
+#         file_import_df['Category'] = 'Uncategorized'
+#     except ValueError as e:
+#         print(f"Error reading file: {e}")
+#         st.error("Error reading file, please ensure the following columns are present - Date, Description, Amount")
+#         st.stop()
 
-# print(completion.choices[0].message);
+#     print('file_import_df')
+#     print(file_import_df)
 
+#     for index, row in file_import_df.iterrows():
+#         label = str(openai_classification(row['Description']))
+#         print(label)
+#         # file_import_df.at[index, 'Description'] = label
+#         print(str(row['Description']) + ' - ' + label)
+#         time.sleep(0.5)
 
-
-uploaded_file = st.file_uploader("Please ensure the file is in CSV format and contains the required columns as Date, Description, Amount", label_visibility ="visible", help="Upload a CSV file with the required columns", key='uploaded_file')
-if uploaded_file is not None:
-    try:
-        file_import_df = pd.read_csv(uploaded_file, usecols=['Date', 'Description', 'Amount'])
-        file_import_df['Category'] = 'Uncategorized'
-    except ValueError as e:
-        print(f"Error reading file: {e}")
-        st.error("Error reading file, please ensure the following columns are present - Date, Description, Amount")
-        st.stop()
-
-    print('file_import_df')
-    print(file_import_df)
-
-    for index, row in file_import_df.iterrows():
-        label = str(openai_classification(row['Description']))
-        # file_import_df.at[index, 'Description'] = label
-        print(str(row['Description']) + ' - ' + label)
-        time.sleep(0.5)
+capture_text =  st.text_input("Enter a description", key="description_input")
+if capture_text:
+    label = str(openai_classification(capture_text))
+    print(label)
+    st.write("Label: ", label)
+    time.sleep(0.5)
